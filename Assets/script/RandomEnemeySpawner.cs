ff8a15e86acf43c1d428f14721199732dd89a531
Luc <1017799@hr.nl>
2023-06-18 14:59:47 +0200

    Added intro and outro
    - Added intro scene and timeline
    - Added outro scene and timeline
    - Added autoloading of new scene on completion of previous

--- Assets/script/RandomEnemeySpawner.cs
+++ Assets/script/RandomEnemeySpawner.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RandomEnemySpawner : MonoBehaviour
 {
@@ -48,6 +49,7 @@ public class RandomEnemySpawner : MonoBehaviour
         if (transform.childCount == 0)
         {
             Debug.Log("All enemies are dead");
+            SceneManager.LoadScene("OutroScene", LoadSceneMode.Single);
         }
     }
 }
